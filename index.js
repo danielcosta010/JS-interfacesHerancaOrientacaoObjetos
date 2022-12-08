641309b472242bcebc04c29d463d5bc9eeec2255
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2022-12-08 00:23:09 -0300

    Heranca

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
-import { Conta } from "./Conta.js";
+
 const cliente1 = new Cliente("Daniel", 11122233309);
 
 
 
-const contaCorrenteDaniel = new Conta(0, cliente1, 1001);
+const contaCorrenteDaniel = new ContaCorrente (cliente1, 1001);
 contaCorrenteDaniel.depositar(500);
 contaCorrenteDaniel.sacar(100);
 
-const contaPoupanca = new Conta(100, cliente1, 1001);
+const contaPoupanca = new ContaPoupanca (100, cliente1, 1001);
+contaPoupanca.sacar(10)
 
 console.log(contaPoupanca);
 
 console.log(contaCorrenteDaniel);
+
