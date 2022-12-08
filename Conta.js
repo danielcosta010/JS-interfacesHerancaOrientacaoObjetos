e743e4ccca1136c7b90b95064fcab476fcaab797
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2022-12-08 01:38:41 -0300

    Classes abstratas

--- Conta.js
+++ Conta.js
@@ -1,8 +1,12 @@
 export class Conta {
   constructor(saldoInicial, cliente, agencia) {
+    if(this.constructor == Conta) {
+      throw new Error('Você não deveria instanciar um objeto do tipo Conta Diretamente, pois está é uma classe abstrata');
+    }
     this._saldo = saldoInicial;
     this._cliente = cliente;
     this._agencia = agencia;
+
   }
 
   set cliente(novoValor){
@@ -19,11 +23,10 @@ export class Conta {
       return this._saldo;
   }
 
-  sacar(valor){
 
-    let taxa = 1;
-    return this._sacar(valor, taxa)
-    
+  // Método abstrato
+  sacar(valor){
+    throw new Error ('O método sacar da conta é abstrato')
   }
 
   _sacar(valor, taxa) {
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
+import { ContaSalario } from "./ContaSalario.js";
 
 const cliente1 = new Cliente("Daniel", 11122233309);
 
@@ -11,9 +12,15 @@ contaCorrenteDaniel.depositar(500);
 contaCorrenteDaniel.sacar(100);
 
 const contaPoupanca = new ContaPoupanca (100, cliente1, 1001);
-contaPoupanca.sacar(10)
+contaPoupanca.sacar(10);
+
+const contaSalario = new ContaSalario(cliente1);
+contaSalario.depositar(100);
+contaSalario.sacar(10)
+
 
 console.log(contaPoupanca);
 
 console.log(contaCorrenteDaniel);
 
+console.log(contaSalario);
